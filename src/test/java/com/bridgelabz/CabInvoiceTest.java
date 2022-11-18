abb47cc3351dcb4be3a7f50f72f188ddde92373d
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-18 15:23:44 +0530

    Added : Cab Invoice Generate Enhanced Invoice

--- src/test/java/com/bridgelabz/CabInvoiceTest.java
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -1,4 +1,4 @@
-// step2 : Cab Invoice Generate Multiple Rides
+// step3 : Cab Invoice Generate Enhanced Invoice
 package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
@@ -24,5 +24,15 @@ public class CabInvoiceTest
         Ride[] rides ={new Ride(12.0,3.0),new Ride(14.0,5.0),new Ride(0.1,3.0)};
         double totalFare = cabinvoice.getTotalFare(rides);
         Assertions.assertEquals(273,totalFare);
+    }
+    @Test
+    public void givenMultipleRidesShould_ReturnInvoice()
+    {
+        CabInvoice cabinvoice = new CabInvoice();
+        Invoice invoice = new Invoice();
+        Ride[] rides ={new Ride(12.0,3.0),new Ride(14.0,5.0),new Ride(0.1,3.0)};
+        Invoice actualInvoice = cabinvoice.getInvoiceOfRides(rides);
+        Invoice expectedInvoice = new Invoice(3,273,91);
+        Assertions.assertEquals(expectedInvoice,actualInvoice);
     }
 }
