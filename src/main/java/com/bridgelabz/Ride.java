67176560f25143f416f0efbd418d04c64171fcb3
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-18 16:02:17 +0530

    Added : Cab Invoice Generate Premium Rides (Bonus)

--- src/main/java/com/bridgelabz/Ride.java
+++ src/main/java/com/bridgelabz/Ride.java
@@ -5,9 +5,15 @@ public class Ride
 {
     double distance;
     double time;
-    public Ride(double distance, double time)
-    {
+    String rideType;
+    public Ride(double distance, double time) {
         this.distance = distance;
         this.time = time;
+    }
+
+    public Ride(double distance, double time, String rideType) {
+        this.distance = distance;
+        this.time = time;
+        this.rideType = rideType;
     }
 }
