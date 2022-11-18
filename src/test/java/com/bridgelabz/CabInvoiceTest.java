30cd1c79453b89bd5179861e6d37938b94b88edf
NagrajPanchal <nagrajsubhashpanchal@gmail.com>
2022-11-18 10:50:46 +0530

    Added : Calculate Fare

---
+++ src/test/java/com/bridgelabz/CabInvoiceTest.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CabInvoiceTest
+{
+    @Test
+    public void givenTimeAndDistanceShould_ReturnTotalFare(){
+        CabInvoice cabinvoice = new CabInvoice();
+        double totalFare = cabinvoice.getTotalFare(10,1);
+        Assertions.assertEquals(101,totalFare);
+    }
+    @Test
+    public void givenTimeAndDistanceShould_ReturnMinimumFare(){
+        CabInvoice cabinvoice = new CabInvoice();
+        double totalFare = cabinvoice.getTotalFare(0.2,1);
+        Assertions.assertEquals(5,totalFare);
+    }
+}
